fa4d52a0d07f0b9c8101d76a705c6931cb11d5f7
Stuart Henry <stuarthenry@gmail.com>
2008-11-18 20:04:49 -0800

    added googlecharts as a plugin

--- app/controllers/ferms_controller.rb
+++ app/controllers/ferms_controller.rb
@@ -1,5 +1,4 @@
 class FermsController < ApplicationController
-  require 'gchart'
   
   def index
   end
@@ -10,9 +9,10 @@ class FermsController < ApplicationController
   
   def detail
     @chart = Gchart.line_xy(:size => '500x400',
-                            :bg => '999999',
+                            :bg => 'ffffff',
                             :data => [[10,25,38,60,90],[50,60,80,90,99],[10,15,20,35,55],[80,60,40,20,1]],
-                            :line_colors => ['ffff00', 'bec0c0'])
+                            :line_colors => ['ff0000cc', '0000ffcc']
+                            )
   end
 
   def new
